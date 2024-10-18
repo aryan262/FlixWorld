8ca350c896bf30af5e1e0336e8d1ac46ec49ae52
Aryan Panchal <aryan.nikolas@gmail.com>
2024-10-18 19:47:25 +0530

    "Removed trailing slash from CORS origin URL"

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: 'http://localhost:5173/' , credentials:true}));
+app.use(cors({origin: 'http://localhost:5173' , credentials:true}));
 app.use(express.urlencoded({extended:true}))
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
